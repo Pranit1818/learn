082c36171189081356dc6507b4ca4a7cceb29358
Prasad Kulkarni <prasadkulkarni2027@gmail.com>
2026-02-04 00:56:38 +0530

    changes new

--- sum.java
+++ sum.java
@@ -2,10 +2,10 @@ public class Sum {
     public static void main(String[] args) {
         
         int a = 10;
-        int b = 20;
+        int b =2240;
         
         int sum = a + b;
-        
+        //hdeufnahf ndhifowf n
         System.out.println("Sum is = " + sum);
     }
 }
